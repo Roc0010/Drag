99d7b909a66cad506289e72e091d17f571927dcc
Starlight-Roc <cheng.peng@starlight-sms.com>
2015-07-01 18:00:35 +0800

    add feature

--- Drag/Drag/ViewController.swift
+++ Drag/Drag/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tSlider: UISlider! //目标
+    @IBOutlet weak var mSlider: UISlider! //可以拖动
+    @IBOutlet weak var scoreLabel: UILabel! //分数
+    @IBOutlet weak var roundLabel: UILabel! //回合
+    
+    var targetValue = 0
+    var currentValue = 0
+    var score = 0;
+    var round = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sliderChanged(sender: UISlider) {
+    }
 
+    @IBAction func clickAction(sender: UIButton) {
+    }
+    
+    @IBAction func roleAction(sender: UIButton) {
+    }
+    
+    @IBAction func restartAction(sender: UIButton) {
+    }
 }
+
 
